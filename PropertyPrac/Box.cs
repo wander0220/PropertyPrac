b7af6f1697815284e92cd3cb29360cae46e0bb53
wander0220 <s2018s26@e-mirim.hs.kr>
2020-05-29 10:38:36 +0900

    IsPositive()를 만듦

--- PropertyPrac/Box.cs
+++ PropertyPrac/Box.cs
@@ -9,30 +9,16 @@ namespace PropertyPrac
 {
     class Box
     {
-        const int WIDTH_DEFAULT = 1;
-        const int HEIGHT_DEFAULT = 1;
+        const int SIZE_DEFAULT = 1;
         int width;
         int height;
 
-        public Box(int width,int height)
+        public Box(int width, int height)
         {
-            if (width > 0 && height > 0)
-            {
-                this.width = width;
-                this.height = height;
-            }
-            else
-            {
-                this.width = 1;
-                this.height = 1;
-                Console.WriteLine("매개변수 is 양수... no 음수...");
-            }
+            this.width = IsPositive(width);
+            this.height = IsPositive(height);
         }
-        public int Area()
-        {
-            return this.width * this.height;
-        }
-
+     
         public int GetWidth()
         {
             return this.width;
@@ -43,28 +29,31 @@ namespace PropertyPrac
         }
         public void SetWidth(int width)
         {
-            if (width > 0)
-            {
-                this.width = width;
-            }
-            else
-            {
-                this.width = WIDTH_DEFAULT;
-                Console.WriteLine("매개변수 is 양수... no 음수...");
-                Console.WriteLine("width",WIDTH_DEFAULT,"height",HEIGHT_DEFAULT," 1인 객체로 수정합니다.");
-            }
+            this.width = IsPositive(width);
         }
         public void SetHeight(int height)
         {
-            if (height > 0)
+            this.height = IsPositive(height);
+        }
+
+        private int IsPositive(int value)
+        {
+            if (value > 0)
             {
-                this.height = height;
+                return value;
             }
             else
             {
-                this.height = HEIGHT_DEFAULT;
                 Console.WriteLine("매개변수 is 양수... no 음수...");
-                Console.WriteLine("width", WIDTH_DEFAULT, "height", HEIGHT_DEFAULT, " 1인 객체가 수정합니다.");
+                Console.WriteLine("기본 값인 ",SIZE_DEFAULT,"인 객체로 수정합니다.");
+                return SIZE_DEFAULT;
             }
         }
+
+        public int Area()
+        {
+            return this.width * this.height;
+        }
+
     }
+}
