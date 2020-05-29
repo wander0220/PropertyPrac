c9b10580790548b5eae5fb2425c65add88a68d05
wander0220 <s2018s26@e-mirim.hs.kr>
2020-05-29 10:28:28 +0900

    ...choi gi commit?

---
+++ PropertyPrac/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPrac
+{
+    class Box
+    {
+        int width;
+        int height;
+
+        public Box(int width,int height)
+        {
+            if (width > 0 && height > 0)
+            {
+                this.width = width;
+                this.height = height;
+            }
+            else
+            {
+                Console.WriteLine("매개변수 is 양수... no 음수...");
+            }
+        }
+        public int Area()
+        {
+            return this.width * this.height;
+        }
+    }
+}
---
+++ PropertyPrac/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int width = 0;
+            //int height = 0;
+            Box box = new Box(10, 10);
+            Console.WriteLine(box.Area());
+            box = new Box(-5, 10);
+            Console.WriteLine(box.Area());
+        }
+    }
+}
